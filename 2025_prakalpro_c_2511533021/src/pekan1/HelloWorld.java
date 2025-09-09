bdbbe365df7a62f4185fad17813bdea62e73cf84
HP <HP@FTI-LAB03-PC09>
2025-09-09 11:58:38 +0700

    praktikum 1 kelas C

---
+++ 2025_prakalpro_c_2511533021/src/pekan1/HelloWorld.java
@@ -0,0 +1,16 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+//		program pertama 
+		
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("program ini menghasilkan");
+		System.out.println("Empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+        System.out.println("This"+"program prints a\n"+"quote from Gettysburg Address.");
+	}
+
+}
