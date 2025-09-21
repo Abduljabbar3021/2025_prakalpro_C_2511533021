8fb0212b1d7e00ef7ae4d14ff8d98f9fcb95af7d
acer <acer@LAPTOP-VF5R2U1P>
2025-09-21 13:04:06 +0700

    tugas pekan 2

---
+++ 2025_prakalpro_c_2511533021/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,26 @@
+package pekan2;
+
+public class tugasAlproPekan2{
+
+	public static void main(String[] args) {
+		int umur = 20;
+		float tinggi  =170.5f;
+		char golonganDarah = 'o';
+		boolean butaWarna = false;
+		
+		System.out.println("=== Data Diri ===");
+		System.out.println("umur: " + umur +"tahun");
+		System.out.println("tinggi: "+ tinggi +"cm");
+		System.out.println("golongan darah: "+ golonganDarah);
+		System.out.println("tinggi: "+ tinggi +"cm");
+		
+		if (butaWarna) {
+			System.out.println("keterangan: anda buta warna");
+		} else {
+			System.out.println("keterangan: anda tidak buta warna");
+		}
+		
+
+	}
+
+}
