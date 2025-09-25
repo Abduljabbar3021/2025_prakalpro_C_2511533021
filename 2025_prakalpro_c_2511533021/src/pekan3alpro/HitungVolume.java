eefd0ea0ccff0d6cfe0caae7ce54f59bfc21404e
acer <acer@LAPTOP-VF5R2U1P>
2025-09-25 12:53:27 +0700

    TugasAlproPekan3

---
+++ 2025_prakalpro_c_2511533021/src/pekan3alpro/HitungVolume.java
@@ -0,0 +1,21 @@
+package pekan3alpro;
+
+import java.util.Scanner;
+
+public class HitungVolume {
+
+	public static void main(String[] args) {
+		int r;
+		double VolumeBola;
+		double phi = 3.14;
+		Scanner keyboard = new Scanner (System.in); 
+		System.out.print("input r atau jari-jari Bola: ");
+		r = keyboard.nextInt();
+		keyboard.close();
+		System.out.print("Rumus Volume Bola : 4.0/3.0 * phi * r * r * r ");
+		VolumeBola = 4.0/3.0 * phi * r * r * r;
+		System.out.println("Volume Bola Adalah = " +VolumeBola);
+
+	}
+
+}
