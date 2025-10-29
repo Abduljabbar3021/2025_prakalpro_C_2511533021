e7bccad11ce118ad93ba79c0d03a5b6f9c98738e
Abduljabbar3021 <akbarjabbar632@gmail.com>
2025-10-29 21:33:42 +0700

    Add files via upload

--- 2025_prakalpro_c_2511533021/src/pekan3_2511533021/OperatorRelasional_2511533021.java
+++ 2025_prakalpro_c_2511533021/src/pekan3_2511533021/OperatorRelasional_2511533021.java
@@ -1,9 +1,32 @@
 package pekan3_2511533021;
 
+import java.util.Scanner;
+
 public class OperatorRelasional_2511533021 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int A1;
+		int A2;
+		boolean hasil;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Input angka-1: ");
+		A1 = keyboard.nextInt();
+		System.out.print("Input angka-2: ");
+		A2 = keyboard.nextInt();
+		keyboard.close();
+		hasil = A1 > A2;//apakah A1 lebih besar dari A2?
+		System.out.println("A1 > A2 =: " +hasil);
+		hasil = A1 < A2;//apakah A1 lebih kecil dari A2?
+		System.out.println("A1 < A2 =: " +hasil);
+		hasil = A1 >= A2;//apakah A1 lebih besar sama dengan A2?
+		System.out.println("A1 >= A2 =: " +hasil);
+		hasil = A1 <= A2;//apakah A1 lebih kecil sama dengan A2?
+		System.out.println("A1 <= A2 =: " +hasil);
+		hasil = A1 == A2;//apakah A1 sama dengan A2?
+		System.out.println("A1 == A2 =: " +hasil);
+		hasil = A1 != A2;//apakah A1 tidak sama dengan A2?
+		System.out.println("A1 != A2 =: " +hasil);
+
 
 	}
 
