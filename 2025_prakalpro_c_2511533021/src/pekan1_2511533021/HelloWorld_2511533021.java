374a7314757ccf7f8abc8190de55054572b3f5c1
Abduljabbar3021 <akbarjabbar632@gmail.com>
2025-10-29 19:48:15 +0700

    Add files via upload

---
+++ 2025_prakalpro_c_2511533021/src/pekan1_2511533021/HelloWorld_2511533021.java
@@ -0,0 +1,10 @@
+package pekan1_2511533021;
+
+public class HelloWorld_2511533021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 2025_prakalpro_c_2511533021/src/pekan1_2511533021/MembuatKue_2511533021.java
@@ -0,0 +1,10 @@
+package pekan1_2511533021;
+
+public class MembuatKue_2511533021 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
