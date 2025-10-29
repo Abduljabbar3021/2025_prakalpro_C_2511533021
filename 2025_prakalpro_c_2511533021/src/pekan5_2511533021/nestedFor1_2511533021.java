31992c1b9dbd16b81d85a21b3e519b986cb49915
Abduljabbar3021 <akbarjabbar632@gmail.com>
2025-10-29 18:48:57 +0700

    Add files via upload

---
+++ 2025_prakalpro_c_2511533021/src/pekan5_2511533021/nestedFor1_2511533021.java
@@ -0,0 +1,17 @@
+package pekan5_2511533021;
+
+public class nestedFor1_2511533021 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++)
+			for(int j=1; j<=5; j++) {
+				System.out.println();
+			
+			}
+		    System.out.println();
+			
+
+
+	}
+
+}
