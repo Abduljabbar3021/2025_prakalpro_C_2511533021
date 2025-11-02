a0886e1ce59fbe6ca8870198564726163f016e32
Abduljabbar3021 <akbarjabbar632@gmail.com>
2025-11-02 13:20:47 +0700

    Add files via upload

---
+++ 2025_prakalpro_c_2511533021/src/pekan5_2511533021/perulanganFor1_2511533021.java
@@ -0,0 +1,12 @@
+package pekan5_2511533021;
+
+public class perulanganFor1_2511533021 {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=10;i++)
+			System.out.println(i);
+	
+
+	}
+
+}
---
+++ 2025_prakalpro_c_2511533021/src/pekan5_2511533021/perulanganFor_2511533021.java
@@ -0,0 +1,25 @@
+package pekan5_2511533021;
+import java.util.Scanner;
+public class perulanganFor_2511533021 {
+
+	public static void main(String[] args) {
+		int jumlah=0;
+		int batas;
+		Scanner input= new Scanner(System.in);
+		System.out.print("Masukkan nilai batas =");
+		batas=input.nextInt();
+		input.close();
+		for (int i=1;i<=batas;i++) {
+			System.out.print(i);
+			jumlah=jumlah+i;
+			if(i<batas) {
+				System.out.print(" + ");
+		} else{
+			System.out.print(" = ");
+		}
+	}
+		System.out.println(jumlah);
+		
+	}
+	}		
+
